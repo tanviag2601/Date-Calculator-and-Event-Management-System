33a9e8872be9b61a0dc04162ea555173d20a13cd
Tanvi <tanvi.agarwal.2601@gmail.com>
2026-09-18 12:46:00 +0530

    Added Date class
    Date class for storing and managing date values

---
+++ Date.java
@@ -0,0 +1,58 @@
+public class Date
+{
+    private int day;
+    private int month;
+    private int year;
+
+    // Constructor
+    public Date(int day, int month, int year)
+    {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    // Getters
+    public int getDay()
+    {
+        return day;
+    }
+
+    public int getMonth()
+    {
+        return month;
+    }
+
+    public int getYear()
+    {
+        return year;
+    }
+
+    // Setters
+    public void setDay(int day)
+    {
+        this.day = day;
+    }
+
+    public void setMonth(int month)
+    {
+        this.month = month;
+    }
+
+    public void setYear(int year)
+    {
+        this.year = year;
+    }
+
+    // Display date
+    public void displayDate()
+    {
+        System.out.println(day + "/" + month + "/" + year);
+    }
+
+    // Convert date to String
+    public String toString()
+    {
+        return day + "/" + month + "/" + year;
+    }
+}
